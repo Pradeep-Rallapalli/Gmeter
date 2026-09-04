8c124091786e1b54d07e20d24fc5346944c692e2
Pradeep-Rallapalli <pradeep.rgs@icloud.com>
2026-09-04 11:19:32 -0700

    Initial Commit

---
+++ Gmeter/GmeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GmeterApp.swift
+//  Gmeter
+//
+//  Created by Pradeep Rallapalli on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct GmeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
