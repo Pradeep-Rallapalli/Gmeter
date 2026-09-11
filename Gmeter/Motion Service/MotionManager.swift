595091bff0277f73ca8b8a54405570f00e25c57f
Pradeep-Rallapalli <pradeep.rgs@icloud.com>
2026-09-11 15:24:27 -0700

    Addd basic averaging filter to motion manager. Reticle movements are now more stable and less jittery.
    Work on: Reticle trail animation.

--- Gmeter/Motion Service/MotionManager.swift
+++ Gmeter/Motion Service/MotionManager.swift
@@ -11,10 +11,16 @@ import CoreMotion
 @Observable
 class MotionManager {
     
-    var motionManager = CMMotionManager()
+    private let windowSize = 8
+    
+    private var motionManager = CMMotionManager()
     
     var accelY: Double = 0
     var accelX: Double = 0
+    
+    var accelXBuf: [Double] = []
+    var accelYBuf: [Double] = []
+    
     var highestG: Double = 0
     var currentG: Double = 0
     
@@ -24,13 +30,28 @@ class MotionManager {
             guard let newAccel = data?.userAcceleration else { return }
             guard let self else { return }
             
-            self.accelX = newAccel.x
-            self.accelY = newAccel.y
-            
-            self.currentG = sqrt((newAccel.x * newAccel.x) + (newAccel.y * newAccel.y))
             
             
             
+            if(self.accelXBuf.count < windowSize) {
+                self.accelXBuf.append(newAccel.x)
+            } else {
+                self.accelXBuf.removeFirst()
+                self.accelXBuf.append(newAccel.x)
+            }
+            
+            if(self.accelYBuf.count < windowSize) {
+                self.accelYBuf.append(newAccel.y)
+            } else {
+                self.accelYBuf.removeFirst()
+                self.accelYBuf.append(newAccel.y)
+            }
+            
+            self.accelX = self.accelXBuf.reduce(0, +) / Double(self.windowSize)
+            self.accelY = self.accelYBuf.reduce(0, +) / Double(self.windowSize)
+
+            self.currentG = sqrt((self.accelX * self.accelX) + (self.accelY * self.accelY))
+
             if self.currentG > self.highestG {
                 self.highestG = self.currentG
             }
